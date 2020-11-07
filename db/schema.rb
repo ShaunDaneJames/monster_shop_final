4935a700b7ed35a55ceb81afe5013629ed5d0f28
Shaun James <ShaunDaneJames@Gmail.com>
2020-11-07 10:12:24 -0700

    Adds item foreign key to discounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_163004) do
+ActiveRecord::Schema.define(version: 2020_11_07_170959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_11_07_163004) do
     t.float "percentage"
     t.integer "quantity"
     t.bigint "merchant_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_discounts_on_item_id"
     t.index ["merchant_id"], name: "index_discounts_on_merchant_id"
   end
 
@@ -91,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_11_07_163004) do
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
+  add_foreign_key "discounts", "items"
   add_foreign_key "discounts", "merchants"
   add_foreign_key "items", "merchants"
   add_foreign_key "order_items", "items"
