46ae3e760053325bb334be2eae34e04fbef631ac
Shaun James <ShaunDaneJames@Gmail.com>
2020-11-07 11:44:21 -0700

    WIP creating new discount for a merchant

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,12 +1,13 @@
 class DiscountsController < Merchant::BaseController
 
   def new
-    @discount = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+    # @discount = Merchant.find(params[:merchant_id])
   end
 
   def create
     merchant = Merchant.find(params[:merchant_id])
-    discount = merchant.discounts.new({
+    discount = merchant.discounts.create({
       percentage: params([:percentage]),
       quantity: params([:quantity])
       })
