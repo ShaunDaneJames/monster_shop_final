6307e6aa4895784a60bd3a280dbf4f4a1da30626
Shaun James <ShaunDaneJames@Gmail.com>
2020-11-10 22:15:28 -0700

    Adds model methods for finding and apply discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,4 +2,7 @@ class Discount < ApplicationRecord
 
  belongs_to :merchant
  validates_presence_of :percentage, :quantity
+
+ validates_numericality_of :percentage, greater_than: 0, less_than: 100
+ validates_numericality_of :quantity, greater_than: 0
 end
